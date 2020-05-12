f3322991fab41d6b040871b8e2099f8d1148afa8
theovier <Theo@Harkenbusch.com>
2020-05-12 19:24:10 +0200

    add structure for navbar and main content

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,29 @@
+<div class="bg-gray-100 pt-24 lg:pt-0">
+
+    <div class="fixed z-100 bg-gray-100 inset-x-0 top-0 border-b-2 border-gray-200 lg:border-b-0 lg:static flex items-center">
+        <div class="w-full max-w-screen-xl relative mx-auto px-6">
+            <div class="lg:border-b-2 lg:border-gray-200 h-24 flex flex-col justify-center">
+                <div class="flex items-center justify-between">
+
+                    <div class="flex items-center">
+                        <a href="/" class="block">
+                            <h1 class="text-2xl text-gray-800 font-bold">University Credits 4.0</h1>
+                        </a>
+                    </div>
+
+                    <div class="flex items-center text-gray-500">
+                        <a class="block flex items-center hover:text-gray-700 mr-5" href="https://github.com/upb-uc4/">
+                            <i class="fab fa-2x fa-github"></i>
+                        </a>
+                        <a class="block flex items-center hover:text-gray-700 mr-5" href="https://twitter.com/uc4_official">
+                            <i class="fab fa-2x fa-twitter"></i>
+                        </a>
+                        <a class="block flex items-center hover:text-gray-700" href="/discord">
+                            <i class="fab fa-2x fa-discord"></i>
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
