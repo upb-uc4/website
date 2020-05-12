f6c760098f3cc548b7fe8e1764050b9800f4062f
theovier <Theo@Harkenbusch.com>
2020-05-12 18:04:33 +0200

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    purge: [
+        './resources/**/*.html',
+        './resources/**/*.blade',
+        './resources/**/*.vue',
+    ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,11 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .postCss('resources/css/app.css', 'public/css', [
+        require('tailwindcss')('./tailwind.config.js')
+    ]);
+
+mix.browserSync({
+    proxy: 'apache2',
+    port: 3000,
+})
