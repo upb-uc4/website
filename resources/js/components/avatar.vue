81485902d95d2d014b94013f935239169f3e663f
theovier <Theo@Harkenbusch.com>
2020-05-13 09:51:04 +0200

    add example about-us content

--- resources/js/components/avatar.vue
+++ resources/js/components/avatar.vue
@@ -1,9 +1,20 @@
 <template>
-    <div>test</div>
+    <div class="max-w-sm overflow-hidden">
+        <img class="w-48 h-48 mt-2 rounded-full object-cover mx-8" src="https://www.constructdigital.com/hs-fs/hubfs/Minh-1.jpg?width=1200&height=1200&name=Minh-1.jpg">
+        <div class="py-4">
+            <div class="text-center font-bold text-xl">{{ name }} {{ lastname}}</div>
+            <div class="text-center text-gray-500">{{ title }}</div>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "avatar.vue"
+        name: "avatar.vue",
+        props: [
+            'name',
+            'lastname',
+            'title',
+        ],
     }
 </script>
