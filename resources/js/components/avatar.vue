1147ac7d45b4ac5fdd2352f134ceb8d279833f7c
theovier <Theo@Harkenbusch.com>
2020-05-14 22:24:34 +0200

    restyle icon size and spacing

--- resources/js/components/avatar.vue
+++ resources/js/components/avatar.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="max-w-sm overflow-hidden">
-        <img class="w-48 h-48 mt-2 rounded-full object-cover mx-8" src="https://www.constructdigital.com/hs-fs/hubfs/Minh-1.jpg?width=1200&height=1200&name=Minh-1.jpg">
-        <div class="py-4">
+        <img class="w-32 h-32 mt-2 rounded-full object-cover mx-16" src="https://www.constructdigital.com/hs-fs/hubfs/Minh-1.jpg?width=1200&height=1200&name=Minh-1.jpg">
+        <div class="my-6">
             <div class="text-center font-bold text-xl">{{ name }} {{ lastname}}</div>
-            <div class="text-center text-gray-500">{{ title }}</div>
+            <div class="text-center text-sm text-gray-500 uppercase font-semibold">{{ title }}</div>
         </div>
     </div>
 </template>
--- resources/views/about/people.blade.php
+++ resources/views/about/people.blade.php
@@ -11,14 +11,14 @@
             <div class="w-1/4 border-b-4 border-blue-500"></div>
         </div>
 
-        <div class="w-1/2 mb-12 mx-auto">
-            <p class="text-gray-800 text-lg text-center font-medium">
+        <div class="w-1/2 mb-24 mx-auto">
+            <p class="text-lg leading-relaxed m-4 text-gray-600 text-center">
                 This is us, the team behind UC4. We are a group of passionate Master's students at the Paderborn University who work hard to make your everyday life at campus more simple.
             </p>
         </div>
 
         <div class="flex justify-center">
-            <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            <div class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-12">
                 @for ($i = 0; $i < 4; $i++)
                     <avatar name="Jon" lastname="Doe" title="Supervisor"></avatar>
                     <avatar name="Jim" lastname="Panse" title="Technical Manager"></avatar>
