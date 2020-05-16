eee7664ebcb3c12a95e85f46b3ce02257d99dc5a
theovier <Theo@Harkenbusch.com>
2020-05-16 15:28:44 +0200

    add anchors for faq sections

--- resources/views/faq/components/section.blade.php
+++ resources/views/faq/components/section.blade.php
@@ -1,5 +1,7 @@
 <div class="mt-10">
-    <h2 class="text-2xl font-bold text-blue-600">{{ $title }}</h2>
+    <h2 class="text-2xl font-bold text-blue-600">
+        <a name="{{ strtolower($title) }}"></a>{{ $title }}
+    </h2>
     @include('faq/components/question', ['question' => "First Question in General?", 'answer' => "Bitcoin ist ein konsensorientiertes Netzwerk, welches ein neues Zahlungssystem und vollständig digitales Geld möglich macht.
         Es ist das erste dezentralisierte Peer-to-Peer Zahlungsnetzwerk, das nur von den Nutzern und ohne zentrale Autorität
         oder Vermittler betrieben wird"])
