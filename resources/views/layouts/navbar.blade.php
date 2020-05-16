8b6437c17edeb5466214f8fa212ce08d973ea303
theovier <Theo@Harkenbusch.com>
2020-05-16 23:55:17 +0200

    redesign navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,35 +1,35 @@
 <div class="bg-gray-100 pt-0 pb-8">
-
-    <div class="w-full bg-blue-500 h-3"></div>
-
     <div class="bg-gray-100 z-100 bg-white flex items-center">
         <div class="w-full max-w-screen-xl relative mx-auto px-6">
-            <div class="border-b-2 border-gray-200 h-16 flex flex-col justify-center">
+            <div class="border-b-2 border-gray-200 h-24 flex flex-col justify-center">
                 <div class="flex items-center justify-between">
 
-                    <div class="flex items-center">
+                    <div class="flex ">
                         <a href="/" class="block">
-                            <h1 class="xl:text-2xl lg:text-xl md:text-lg text-gray-800 font-bold"><span class="text-blue-700">U</span>niversity <span class="text-blue-700">C</span>redits <span class="text-blue-700">4</span>.0</h1>
+                            <h1 class="xl:text-2xl lg:text-xl md:text-lg text-gray-800 font-black"><span class="text-blue-500">University</span> Credits <span class="text-blue-500">4</span>.0</h1>
                         </a>
                     </div>
 
-                    <div class="flex items-center">
-                        <a href="/about/people" class="block">
-                            <p class="tracking-wide text-gray-800 mr-8">About us</p>
+                    <div class="flex items-center text-lg font-medium tracking-wide text-gray-500">
+                        <a href="{{ route('home') }}" class="block">
+                            <p class="mr-8 border-b-4 @if(Route::is('home')) text-blue-500 border-blue-500 hover:text-blue-700 @else border-gray-100 hover:text-gray-700 @endif">Home</p>
                         </a>
-                        <a href="/faq" class="block">
-                            <p class="tracking-wide text-gray-800 mr-8">FAQ</p>
+                        <a href="{{ route('about') }}" class="block">
+                            <p class="mr-8 border-b-4 @if(Route::is('about')) text-blue-500 border-blue-500 hover:text-blue-700 @else border-gray-100 hover:text-gray-700 @endif">About</p>
+                        </a>
+                        <a href="{{ route('faq') }}" class="block">
+                            <p class="mr-8 border-b-4 @if(Route::is('faq')) text-blue-500 border-blue-500 hover:text-blue-700 @else border-gray-100 hover:text-gray-700 @endif">FAQ</p>
                         </a>
                     </div>
 
-                    <div class="flex items-center text-gray-500">
-                        <a class="block flex items-center hover:text-gray-700 mr-5" href="https://github.com/upb-uc4/">
+                    <div class="flex text-gray-500">
+                        <a class="block flex hover:text-gray-700 mr-5" href="https://github.com/upb-uc4/">
                             <i class="fab fa-2x fa-github"></i>
                         </a>
-                        <a class="block flex items-center hover:text-gray-700 mr-5" href="https://twitter.com/uc4_official">
+                        <a class="block flex hover:text-gray-700 mr-5" href="https://twitter.com/uc4_official">
                             <i class="fab fa-2x fa-twitter"></i>
                         </a>
-                        <a class="block flex items-center hover:text-gray-700" href="/discord">
+                        <a class="block flex hover:text-gray-700" href="/discord">
                             <i class="fab fa-2x fa-discord"></i>
                         </a>
                     </div>
--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('/faq', 'FaqController@index')->name('faq');
 
 Route::get('/about/people', function () {
     return view('about.people');
-});
+})->name('about');
