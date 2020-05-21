13a900fcf9e0935601ae37e9650d716abb666ae0
theovier <Theo@Harkenbusch.com>
2020-05-21 15:48:32 +0200

    add basic footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,19 @@
 <div class="w-full bg-gray-100">
-
+    <div class="bg-gray-100 z-100 bg-white flex items-center">
+        <div class="w-full max-w-screen-xl mx-auto px-6">
+            <div class="border-t-2 border-gray-200 h-24 flex flex-col justify-center">
+                <div class="flex items-center justify-center">
+                    <div class="flex items-end text-md font-medium tracking-wide text-gray-500">
+                        <p class="mr-8">&copy; University Credits 4.0</p>
+                        <a href="" class="block">
+                            <p class="mr-8 hover:text-gray-700">Datenschutz</p>
+                        </a>
+                        <a href="" class="block">
+                            <p class="mr-8 hover:text-gray-700">Impressum</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
