36ccf8a5ad279d6070f609caa20fa5d05a4da9f4
theovier <Theo@Harkenbusch.com>
2020-06-03 18:42:11 +0200

    mv landing page to vue component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 require('./bootstrap');
 
 Vue.component('avatar', require('./components/avatar.vue').default);
+Vue.component('landing-page', require('./components/landingpage.vue').default);
 
 const app = new Vue({
     el: '#app',
