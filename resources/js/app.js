d5d7cbfacfee5be4a0e4f5f344c9f3de3459130d
theovier <Theo@Harkenbusch.com>
2020-06-18 17:31:07 +0200

    disable debug mode for vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,10 @@ Vue.component('avatar', require('./components/avatar.vue').default);
 Vue.component('landing-page', require('./components/landingpage.vue').default);
 Vue.component('navbar', require('./components/navbar.vue').default);
 
+Vue.config.devtools = false;
+Vue.config.debug = false;
+Vue.config.silent = true;
+
 const app = new Vue({
     el: '#app',
 });
